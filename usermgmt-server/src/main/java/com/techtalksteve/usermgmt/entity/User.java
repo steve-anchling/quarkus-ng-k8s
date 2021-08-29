d786e153e99feb1238641e4635124895a49d0b29
outsourcedtonature <outsourcedtonature@gmail.com>
2021-08-29 12:56:24 +0200

    Add Quarkus server

---
+++ usermgmt-server/src/main/java/com/techtalksteve/usermgmt/entity/User.java
@@ -0,0 +1,50 @@
+package com.techtalksteve.usermgmt.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "USER")
+@Getter
+@Setter
+@ToString(callSuper=true)
+@NoArgsConstructor
+@AllArgsConstructor
+@SequenceGenerator(name=User.USER_SEQ, sequenceName = User.USER_SEQ, allocationSize = 1)
+public class User implements Serializable {
+    public static final String USER_SEQ = "USER_SEQ";
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator=USER_SEQ)
+    @Setter(AccessLevel.PRIVATE)
+    private Long id;
+
+    @NotNull
+    @Column(name = "FIRSTNAME")
+    protected String firstname;
+
+    @NotNull
+    @Column(name = "LASTNAME")
+    protected String lastname;
+
+    @NotNull
+    @Column(name = "BIRTHDATE")
+    private OffsetDateTime birthdate;
+
+
+}
